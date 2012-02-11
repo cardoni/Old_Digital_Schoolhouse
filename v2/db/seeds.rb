b7f2f474ee99342576fc5a85666738e7282a0644
Greg Cardoni <greg@cardoni.net>
2012-02-11 17:03:23 -0600

    seeds file

--- v2/db/seeds.rb
+++ v2/db/seeds.rb
@@ -10,5 +10,3 @@ schools = School.create( [ { name: "Hanoi School", country: "Vietnam", state: "H
 users = User.create([ { username: "Joe Honduras", first_name: "Joe", last_name: "Honduras", email: "joe@schmoe.com", password: "password", active_author: true, school_id: schools.first, bio: "I love Vietnam" }, { username: "JoeThailand", first_name: "Joe", last_name: "Thailand", email: "joe@thailand.com", password: "password", active_author: true, school_id: schools.last, bio: "I am in Thailand." } ])
 
 articles = Article.create([ { article_date: "2012-02-11 22:06:00", published: true, article_title: "Vietnam Video", article_body: "Video in Vietnam", article_tags: "vietnam", user_id: users.first, attachment_URL: "http://youtube.co", attachment_Description: "Video", attachment_Credits: "I did this" }, { article_date: "2012-02-11 22:06:00", published: true, article_title: "Thailand is awesome", article_body: "Thailand is awesome! Way better than Burma!", article_tags: "Thailand Burma", user_id: users.last, attachment_URL: "www.yahoo.com", attachment_Description: "Text", attachment_Credits: "I wrote dat" } ] )
-
-
