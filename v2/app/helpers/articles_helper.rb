20924440f2710471a0e9c820e747ac50b253af93
Adam Mark <adam.w.mark@gmail.com>
2012-02-12 09:26:47 -0600

    Worked on Sessions and some css

--- v2/app/helpers/articles_helper.rb
+++ v2/app/helpers/articles_helper.rb
@@ -1,5 +1,17 @@
 module ArticlesHelper
   
+  def photo_helper(link)
+    if excerpt(link, "outub", :radius => 2) == "youtube"
+      "<object width=\"241\" height=\"145\">
+        <param name=\"movie\" value=\"http://www.youtube.com/e/#{link}\">
+        <param name=\"allowFullScreen\" value=\"true\">
+        <param name=\"allowscriptaccess\" value=\"always\">
+        <embed src=\"http://www.youtube.com/e/#{link}\" type=\"application/x-shockwave-flash\" width=\"241\" height=\"145\" allowscriptaccess=\"always\" allowfullscreen=\"true\">
+      </object>"
+    else
+      image_tag(link)
+    end
+  end
   # @article.attachment_url 
   # if attachment_url ""
 
