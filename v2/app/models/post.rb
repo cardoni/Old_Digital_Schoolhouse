0f37cfaf878ece2a2c3070093f2bafeba7a300ad
Adam Mark <adam.w.mark@gmail.com>
2012-02-11 23:39:40 -0600

    Changed for front end

--- v2/app/models/post.rb
+++ v2/app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+
+  belongs_to :users
+  has_many :attachments
+  
 end
--- v2/app/models/school.rb
+++ v2/app/models/school.rb
@@ -1,2 +1,6 @@
 class School < ActiveRecord::Base
+
+  has_many :users
+  has_many :posts :through => :users
+
 end
