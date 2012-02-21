1ff5cf5ffcfa7e05c6b064a05a4b4a61d955b750
Greg Cardoni <greg@cardoni.net>
2012-02-21 14:31:02 -0600

    new empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalSchoolhouse::Application.config.secret_token = '7fb216b9848e26506be9c6d63af1dc0f988ab7cc0a49707ba782da73a31e558a734fc77b5b6b62a06fd4eb14743a5bcecf94f36bbd3592981259809a0d0a5d29'
