0c5deb6153002e51a55dc13f211b354c14b08c81
Adam Mark <adam.w.mark@gmail.com>
2012-02-21 18:31:12 -0600

    added classification and Haml gem

--- v2/db/schema.rb
+++ v2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120221225139) do
+ActiveRecord::Schema.define(:version => 20120222001552) do
 
   create_table "attachments", :force => true do |t|
     t.string   "provider"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(:version => 20120221225139) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.string   "classification"
   end
 
 end
