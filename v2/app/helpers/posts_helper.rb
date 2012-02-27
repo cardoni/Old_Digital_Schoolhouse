3e803915e006f24d58fdf1780f1750ec85254887
Adam Mark <adam.w.mark@gmail.com>
2012-02-27 17:47:25 -0600

    adjusted nested attributes for post

--- v2/app/helpers/posts_helper.rb
+++ v2/app/helpers/posts_helper.rb
@@ -1,5 +1,11 @@
 module PostsHelper
 
+  def add_attachment_link(name)
+    link_to_function name do |page|
+      page.insert_html :bottom, :attachments, :partial => 'attachment', :object => Attachment.new
+    end
+  end
+  
   def provider_regex(url)
     regex = /www.(youtube)?(be.com)?.*(?:\/|v=)([\w-]+)/ #pulls out "youtube"
     url.match(regex) do
