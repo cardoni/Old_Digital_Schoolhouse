6dc1d7ecac7e812716d911d53a15443a04c1dfa5
Eric MacAdie <Eric@MacAdie.net>
2012-02-27 18:11:35 -0600

    Ticket 15: More validations to the user model and some tests

--- v2/app/models/user.rb
+++ v2/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, :email_format => {:message => 'is not looking good'}
+  validates :email, presence: true, :email_format => {:message => 'is not looking good'},
+    uniqueness: true
 
 end
