e72e3e9453cb9c8279158ff0f1fb1d40af9e921f
Greg Cardoni <greg@cardoni.net>
2012-03-01 17:21:40 -0600

    refactored code that generates "My Posts" view, redesigned "My Posts" view, fixed seeds.rb data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create!(name: 'Emanuel', city: cities.first)
+admin = 1
+adam = 2
+eric = 3
+greg = 4
+teacher = 5
+donor = 6
 
-
-User.create!(name: "Mr. Admin", email: "admin@email.com", classification: "administrator", password_digest: "$2a$10$nM.4xm1gHkVBC25tdSf3xO9N4m7C.SMLWoaSh1.VRml862GOmbnGC")
+User.create!(name: "Admin", email: "admin@email.com", classification: "administrator", password_digest: "$2a$10$nM.4xm1gHkVBC25tdSf3xO9N4m7C.SMLWoaSh1.VRml862GOmbnGC")
 User.create!(name: "Adam", email: "adam@adamsemail.com", classification: "teacher", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
 User.create!(name: "Eric", email: "eric@ericsemail.com", classification: "teacher", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
-User.create!(name: "Bob", email: "bob@bobsemail.com", classification: "student", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
-User.create!(name: "An Admin", email: "admin@admin.com", classification: "administrator", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
-User.create!(name: "Another Admin", email: "admin2@admin.com", classification: "administrator", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
+User.create!(name: "Greg", email: "bob@bobsemail.com", classification: "student", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
 User.create!(name: "Teacher", email: "teacher@email.com", classification: "teacher", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
 User.create!(name: "Donor", email: "donor@email.com", classification: "donor", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
-User.create!(name: "Donor 2", email: "donor2@email.com", classification: "donor", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
-User.create!(name: "Mr Third Donor", email: "donor3@email.com", classification: "donor", password_digest: "$2a$10$l2ZvnTqVQRCjAFqSjyyJhekqXJrAY8HpfMLaJ6h5YBCnLtWPgurIK")
-Post.create!(title: "First Post", body: "First post created by Greg!", user_id: 1)
-Post.create!(title: "Second Post", body: "Another post created by Greg!", user_id: 1)
-Post.create!(title: "Third Post", body: "A post created by Adam!", user_id: 2)
-Post.create!(title: "Third Post", body: "A second post created by Adam!", user_id: 2)
-Post.create!(title: "Fourth Post", body: "A post created by Eric!", user_id: 3)
-Post.create!(title: "Fifth Post", body: "A post created by Bob!", user_id: 4)
-Post.create!(title: "Fifth Post", body: "A second post created by Bob!", user_id: 4)
-Post.create!(title: "Fifth Post", body: "A third post created by Bob!", user_id: 4)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 1, post_id: 1)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 1, post_id: 2)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 2, post_id: 3)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 2, post_id: 3)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 2, post_id: 3)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 3, post_id: 4)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 4, post_id: 5)
-Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: 4, post_id: 6)
-puts "Success: Database cleared and seed.db loaded!"
 
+Post.create!(title: "First Post", body: "First post created by Admin!", user_id: admin)
+Post.create!(title: "Second Post", body: "Another post created by Admin!", user_id: admin)
+Post.create!(title: "Third Post", body: "A post created by Adam!", user_id: adam)
+Post.create!(title: "Fourth Post", body: "A second post created by Adam!", user_id: adam)
+Post.create!(title: "Fifth Post", body: "A post created by Eric!", user_id: eric)
+Post.create!(title: "Sixth Post", body: "A post created by Greg!", user_id: greg)
+Post.create!(title: "Seventh Post", body: "A post created by Teacher!", user_id: teacher)
+Post.create!(title: "Eighth Post", body: "A post created by Doner!", user_id: donor)
+
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: admin, post_id: 1)
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: admin, post_id: 2)
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: adam, post_id: 3)
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: adam, post_id: 3)
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: adam, post_id: 3)
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: adam, post_id: 4)
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: eric, post_id: 5)
+Attachment.create!(provider: "YouTube", attachment_url: "http://www.youtube.com/watch?v=FzRH3iTQPrk", description: "Panda Sneeze video", copyright_info: "No Copyright.", user_id: greg, post_id: 6)
+puts "Success: Seeded!"
