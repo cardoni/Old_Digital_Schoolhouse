2442852b4a7f2b9f374204c4c4d11a5fdc672866
Adam Lupu <adam@codeacademy.org>
2012-03-02 16:26:25 -0600

    Added Admin-only User Creation

--- v2/app/controllers/users_controller.rb
+++ v2/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_filter :login_user!, :only => [:new, :create]
-
+  # skip_before_filter :login_user!, :only => [:new, :create]
+  
   def new
    @user = User.new
   end
