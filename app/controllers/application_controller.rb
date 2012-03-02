ab8e6a909c6aee66a916135c8342234b7903694d
Adam Lupu <adam@codeacademy.org>
2012-03-02 16:26:25 -0600

    Added Admin-only User Creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
   before_filter :login_user!
+  
+  # def :login_user
+  #         if session[:user].blank?
+  #             redirect_to root_url, :notice => "Please sign in."
+  #         end
+  #   end
+  
+    
 end
