704da53ebf15b0cf8f78c12f8e65e307d91aef81
Eric MacAdie <Eric@MacAdie.net>
2012-03-09 13:55:42 -0600

    Ticket 25: Tweet button and Facebook button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,18 @@ function remove_fields(link) {
 	$(link).closest(".image_field").hide('fast');
 }
 
+// Facebook button
+$(function() {
+        likebutton =
+            '<fb:like href="" send="" width="100" show_faces="" ' +
+            'font="" layout="button_count" action="recommend">' +
+        '</fb:like>';
+    $.getScript('http://connect.facebook.net/en_US/all.js', function() {
+        FB.init({appId: 141936272547391,
+                 status: true,
+                 cookie: true,
+                 xfbml: true
+                });
+        $('#facebook').replaceWith(likebutton);
+    });
+});
