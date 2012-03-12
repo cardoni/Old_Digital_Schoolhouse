58d4cc1257f144215f81f4f6631be04bf7b185dd
Eric MacAdie <Eric@MacAdie.net>
2012-03-11 21:10:20 -0500

    Working on hallway view. Still kind of messy

--- v2/app/controllers/home_controller.rb
+++ v2/app/controllers/home_controller.rb
@@ -3,6 +3,21 @@ class HomeController < ApplicationController
   def index
     @posts = Post.find(:all, :order => "created_at desc")
     @title = "Digital Schoolhouse"
+    countries = Country.all.sample(3)
+    # user_ids = countries[0].users.select(:id)
+    # @posts_01 = Post.select(:id).where(:user_id => user_ids)
+    # @posts_01 = Post.where(:user_id => user_ids).limit(1)
+    # @posts_01 = Post.where(:user_id => user_ids)
+    # user_ids = countries[1].users.select(:id)
+    # @posts_02 = Post.where(:user_id => user_ids)
+    # user_ids = countries[2].users.select(:id)
+    # @posts_03 = Post.where(:user_id => user_ids)
+    @array_of_post_arrays = Array.new
+    countries.each do |country|
+      user_ids = country.users.select(:id)
+      @array_of_post_arrays.push(Post.where(:user_id => user_ids))
+    end
+
   end
   
   def about
