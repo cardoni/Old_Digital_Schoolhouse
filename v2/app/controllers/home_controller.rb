5de567370c5097880faa32184d07a6e2199a2cfc
Eric MacAdie <Eric@MacAdie.net>
2012-03-11 22:56:35 -0500

    Ticket 27: Progress on hallway view with horizontal scroll

--- v2/app/controllers/home_controller.rb
+++ v2/app/controllers/home_controller.rb
@@ -15,7 +15,9 @@ class HomeController < ApplicationController
     @array_of_post_arrays = Array.new
     countries.each do |country|
       user_ids = country.users.select(:id)
+      # array: 
       @array_of_post_arrays.push(Post.where(:user_id => user_ids))
+      # hash: @array_of_post_arrays[country.name] = Post.where(:user_id => user_ids)
     end
 
   end
