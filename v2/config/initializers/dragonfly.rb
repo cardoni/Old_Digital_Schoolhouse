a6c22abdc47d220c229b520e7de5c94f07f1514d
Adam Mark <adam.w.mark@gmail.com>
2012-03-16 11:19:32 -0500

    reworked attachments, tweaked some styling, photos are broken for posts temporarily

--- v2/config/initializers/dragonfly.rb
+++ v2/config/initializers/dragonfly.rb
@@ -6,9 +6,9 @@ app.configure_with(:rails)
 app.define_macro(ActiveRecord::Base, :image_df_accessor)
 app.configure do |c|
   c.datastore = Dragonfly::DataStorage::S3DataStore.new({
-    :bucket_name          => 'media.dsch',
-    :access_key_id        => 'AKIAJ6LQSKCQCLRRGSVQ', #AppConfig[:s3_key],
-    :secret_access_key    => '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', #AppConfig[:s3_secret], 
+    :bucket_name          => 'bucket name here',
+    :access_key_id        => 'key goes here', #AppConfig[:s3_key],
+    :secret_access_key    => 'and here', #AppConfig[:s3_secret], 
     # :path                 => 'images '
   })
 
