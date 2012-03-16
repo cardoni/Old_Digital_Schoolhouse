81438a228b985b3a9a9abdc80597e8e8241cddf2
Adam Mark <adam.w.mark@gmail.com>
2012-03-16 11:19:32 -0500

    reworked attachments, tweaked some styling, photos are broken for posts temporarily

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,1 +1,3 @@
 # AppConfig = YAML.load(Rails.root + 'config/app_config.yml')[Rails.env].symbolize_keys!
+
+# This was meant to be used in tandem with config/app_config.yml for easier github use with S3 keys and what not.. hasn't worked for me yet.
