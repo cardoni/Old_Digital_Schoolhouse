3d9da173a7cadd3bea219cd4fbf37605475d4b93
Greg Cardoni <greg@cardoni.net>
2012-03-28 04:31:14 -0500

    removed username/pw before production push; other tweaks

--- v2/app/controllers/application_controller.rb
+++ v2/app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   before_filter :login_user!, except: [:show]
   
-  # TEMPORARY ***  Basic http authentication in production environment
-    if Rails.env.production?
-      http_basic_authenticate_with :name => "secret", :password => "password"
-    end
+  # # TEMPORARY ***  Basic http authentication in production environment
+  #   if Rails.env.production?
+  #     http_basic_authenticate_with :name => "secret", :password => "password"
+  #   end
         
 end
