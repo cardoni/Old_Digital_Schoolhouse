c4ad2630c948ea6126b6c8e5ec38b64a7728017f
Adam Mark <adam.w.mark@gmail.com>
2012-03-27 22:17:19 -0500

    locations schema

--- v2/db/schema.rb
+++ v2/db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120326225311) do
 
   create_table "assets", :force => true do |t|
